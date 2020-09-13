51799a031e59acb0f4479eb87fe64cbb194af163
SBeezy1010 <supremeburton1010@gmail.com>
2020-09-13 23:56:48 +0000

    added a print statement

--- main.swift
+++ main.swift
@@ -71,8 +71,11 @@ print(4 * 9 - 6 / 2)
 print(4 * (9 - 6) / 2)
 
 // If you completed the Fitness Calculations exercise, you calculated an average heart rate to display to the user. However, using proper order of operations you can do this in fewer steps. Create three separate heart rate constants, all of type Double, with values between 60 and 100. Then create a constant equal to the average heart rate. If you use correct order of operations you can do the heart calculation in one line.
-
-
+let heartRates1 = 61.0
+let heartRates2 = 64.0
+let heartRates3 = 99.0
+let heartRatesAverage = 61.0 * 64.0 * 99.0 / 3.0
+print(61.0 * 64.0 * 99.0 / 3.0)
 // One feature you might want to give users is to display their current body temperature. Create a constant tempInFahrenheit equal to 98.6.
 
 
